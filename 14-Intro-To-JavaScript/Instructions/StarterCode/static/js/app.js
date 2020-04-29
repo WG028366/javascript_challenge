d0072d58b8bc44a75b12af9c4183103339bbce27
William <william.garrettiv@outlook.com>
2020-04-29 17:13:53 -0500

    part 1

---
+++ 14-Intro-To-JavaScript/Instructions/StarterCode/static/js/app.js
@@ -0,0 +1,42 @@
+// from data.js
+var tableData = data;
+// YOUR CODE HERE!
+var tbody = d3.select("tbody");
+
+tableData.forEach(function(ufoReport){
+    console.log(ufoReport);
+    var row = tbody.append("tr");
+
+    Object.entries(ufoReport).forEach(function([key, value]){
+        console.log(key, value);
+        var cell = row.append("td");
+        cell.text(value);
+    });
+});
+
+var button = d3.select("#filter-btn")
+var form = d3.select("form")
+
+button.on("click", runEnter);
+form.on("submit", runEnter);
+
+function runEnter() {
+    d3.event.preventDefault();
+    
+    var inputElement = d3.select("#datetime");
+    var inputValue = inputElement.property("value");
+
+    var filteredData = tableData.filter(date => date.datetime === inputValue);
+    console.log(filteredData);
+
+    filteredData.forEach(function(dateSelected){
+        console.log(dateSelected);
+        var row = tbody.append("tr");
+        Object.entries(dateSelected).forEach(function([key, value]){
+            console.log(key, value);
+
+            var cell = row.append("td");
+            cell.text(value);
+        });
+    });
+};
